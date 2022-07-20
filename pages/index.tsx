83fbd2bc4309d77069be62d99baa61a7ac36fbff
Dejan Mircevski <dejan@chiselstrike.com>
2022-07-20 00:08:00 -0400

    Really fix the typing of getSSP
    Signed-off-by: Dejan Mircevski <dejan@chiselstrike.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from 'next'
-import { DocumentContext } from 'next/document'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { GetServerSideProps } from 'next'
 
-export async function getServerSideProps(ctx: DocumentContext) {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
   return {
     props: { ip: ctx.req?.socket.localAddress },
   }
